6e65909231bf1363a2e22c5eff1dbe0d9f47c245
root <root@labredes.teste>
2015-08-13 11:36:01 -0300

    committing changes in /etc after apt run
    Package changes:
    -apport 2.17.2-0ubuntu1.1
    -apport-gtk 2.17.2-0ubuntu1.1
    +apport 2.17.2-0ubuntu1.2
    +apport-gtk 2.17.2-0ubuntu1.2
    -firefox 39.0+build5-0ubuntu0.15.04.1
    -firefox-locale-en 39.0+build5-0ubuntu0.15.04.1
    -firefox-locale-es 39.0+build5-0ubuntu0.15.04.1
    -firefox-locale-fr 39.0+build5-0ubuntu0.15.04.1
    -firefox-locale-pt 39.0+build5-0ubuntu0.15.04.1
    -flashplugin-installer 11.2.202.491ubuntu0.15.04.1
    +firefox 40.0+build4-0ubuntu0.15.04.1
    +firefox-locale-en 40.0+build4-0ubuntu0.15.04.1
    +firefox-locale-es 40.0+build4-0ubuntu0.15.04.1
    +firefox-locale-fr 40.0+build4-0ubuntu0.15.04.1
    +firefox-locale-pt 40.0+build4-0ubuntu0.15.04.1
    +flashplugin-installer 11.2.202.508ubuntu0.15.04.1
    -google-chrome-stable 44.0.2403.130-1
    +google-chrome-stable 44.0.2403.155-1
    -libunity-settings-daemon1 15.04.1+15.04.20150408-0ubuntu1
    +libunity-settings-daemon1 15.04.1+15.04.20150408-0ubuntu1.1
    -linux-firmware 1.143.2
    +linux-firmware 1.143.3
    -python3-apport 2.17.2-0ubuntu1.1
    +python3-apport 2.17.2-0ubuntu1.2
    -python3-problem-report 2.17.2-0ubuntu1.1
    +python3-problem-report 2.17.2-0ubuntu1.2
    -ruby-rack 1.5.2-3
    +ruby-rack 1.5.2-3+deb8u1build0.15.04.1
    -tzdata 2015d-0ubuntu0.15.04
    -tzdata-java 2015d-0ubuntu0.15.04
    +tzdata 2015f-0ubuntu0.15.04
    +tzdata-java 2015f-0ubuntu0.15.04
    -unity-settings-daemon 15.04.1+15.04.20150408-0ubuntu1
    +unity-settings-daemon 15.04.1+15.04.20150408-0ubuntu1.1
    -xul-ext-ubufox 3.0-0ubuntu1
    +xul-ext-ubufox 3.1-0ubuntu0.15.04.1

--- xul-ext/ubufox.js
+++
@@ -1,6 +0,0 @@
-// Place your global Firefox preferences in this file if you are using
-// ubufox. Especially those preferences defined in ubufox need to be configured
-// here to become effective.
-
-// Example: Homepage
-//pref("browser.startup.homepage", "file:/usr/share/doc/xul-ext-ubufox/example-homepage.properties");
